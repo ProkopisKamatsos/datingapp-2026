4edb97be31a02cb32d758d16ec13d4b803e4ee82
Prokopis <prokopiskamatsos@gmail.com>
2026-05-22 10:59:26 +0300

    Add Authentication - Authorization

--- DatingApp/API/Controllers/MembersController.cs
+++ DatingApp/API/Controllers/MembersController.cs
@@ -1,13 +1,16 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+
+
     [Route("api/[controller]")]
     [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet] //localhost:5001/api/members
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
@@ -15,6 +18,7 @@ namespace API.Controllers
             var members = await context.Users.ToListAsync();
             return Ok(members);
         }
+        [Authorize]
         [HttpGet("{id}")] //localhost:5001/api/members/bob-id
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
