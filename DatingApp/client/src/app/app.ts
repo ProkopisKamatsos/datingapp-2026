59d730d79e43107eb7d04b51eefbef54e07650dd
Prokopis <prokopiskamatsos@gmail.com>
2026-05-25 16:09:42 +0300

    Add core features including routing, authentication guard, and toast notifications

--- DatingApp/client/src/app/app.ts
+++ DatingApp/client/src/app/app.ts
@@ -1,43 +1,16 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
-import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected title = 'Dating App';
-  protected members = signal<User[]>([]);
-
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-  setCurrentUser() {
-    const userString = localStorage.getItem('user')!;
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-  async getMembers() {
-    try {
-      return await lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    } catch (error) {
-      console.log(error);
-    }
-    throw new Error('Method not implemented.');
-  }
+export class App {
+  protected router = inject(Router);
 }
