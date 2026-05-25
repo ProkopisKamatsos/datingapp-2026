237634e5d7bd76fab5ef51623b9778799b8fb053
Prokopis <prokopiskamatsos@gmail.com>
2026-05-25 13:57:55 +0300

    Implement user authentication and registration features with navigation

--- DatingApp/client/src/app/app.ts
+++ DatingApp/client/src/app/app.ts
@@ -2,28 +2,39 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
+import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { User } from '../types/user';
+import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
 export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected title = 'Dating App';
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => this.members.set(response),
-      error: error => console.log(error),
-      complete: () => console.log('Request completed')
-    });
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+    this.setCurrentUser();
+  }
+  setCurrentUser() {
+    const userString = localStorage.getItem('user')!;
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
   async getMembers() {
     try {
-      return await lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+      return await lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
     } catch (error) {
       console.log(error);
     }
